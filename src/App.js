6297013bc2d66732a38961d503d44cf906fb4ff6
Diana Garcia-Martinez <dianagarcia1316@gmail.com>
2024-05-14 20:15:50 -0400

    Applying to App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
-
+import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import Header from './Components/Header';
+import Footer from './Components/Footer' ;
+import Home from './Pages/Home';
+import About from './Pages/About';
 
-function App() {
+
+
+const App = () =>{
   return (
+    <Router>
     <div className="App">
+      <Header/>
+      <main>
+        <Switch>
+          <Route exact path='/' Component={Home} />
+          <Route exact path='/about' Component={About} />
+
+
+
+        </Switch>
+
+      </main>
+      <Footer/>
       
     </div>
+    </Router>
   );
+
 }
 
 export default App;
